e0469b70345e04fbc5ede68d3df0710e614e50ee
Pablo Giraldo <pablogirald@yahoo.es>
2020-02-22 10:40:59 +0100

    Add base files

---
+++ src/main/java/info/pablogiraldo/TestSbHerokuApplication.java
@@ -0,0 +1,13 @@
+package info.pablogiraldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSbHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSbHerokuApplication.class, args);
+	}
+
+}
